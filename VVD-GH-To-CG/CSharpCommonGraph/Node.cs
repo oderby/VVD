c33b38a99faebb4d67163508a33301e318ff7219
unknown <aheumann@SEA-L15-JGBBYZ1.nbbj.com>
2015-09-26 13:58:16 -0700

    In-progress GH-to-CG parsing code and a skeleton framework for C# Common graph class

---
+++ VVD-GH-To-CG/CSharpCommonGraph/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCommonGraph
+{
+    public class Node
+    {
+        public String Type;
+        public Guid InstanceGuid;
+        List<Port> Ports;
+        Metadata Metadata;
+
+    }
+
+    public class Metadata
+    {
+        string Ignore;
+        string Inspect;
+    }
+}
---
+++ VVD-GH-To-CG/VVD-GH-To-CG/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GH_IO;
+using System.Xml.Serialization;
+using System.IO;
+using CSharpCommonGraph;
+
+namespace VVD_GH_To_CG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = args[0];
+            Console.WriteLine(filePath);
+            CommonGraph dg = Parser.CommonGraphFromGHFile(filePath);
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(CommonGraph));
+            TextWriter WriteFileStream = new StreamWriter(@"C:\Users\aheumann\Desktop\testOutput.cgx");
+            SerializerObj.Serialize(WriteFileStream, dg);
+ 
+
+            Console.ReadKey();
+
+        }
+    }
+}
